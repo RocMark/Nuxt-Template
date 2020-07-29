c97e44b622a60a1111e3b3d34cc3d27300e2e01b
RocMark <onigir77799@gmail.com>
2020-07-29 23:37:00 +0800

    :wrench: Update ESLint fixOnSave

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,20 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+    extend (config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
   },
   server: {
     port: 3001
