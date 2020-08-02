ac902950d223a64905740face345a6b5c9bdd6dc
RocMark <onigir77799@gmail.com>
2020-08-02 20:54:50 +0800

    :building_construction: ADD utils JS File Example

---
+++ assets/js/format.js
@@ -0,0 +1,2 @@
+export const test = 'testStr123'
+export const test2 = 'testStr456'
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="container">
     <Logo />
-    <h1>RootPage</h1>
+    <h1>RootPage {{ testStr }} </h1>
     <Example />
   </div>
 </template>
 
 <script>
+import { test } from '../assets/js/format'
+
 export default {
+  computed: {
+    testStr () {
+      return test || 'Import Fail'
+    }
+  },
   head () {
     return {
       title: 'CustomTitle',
